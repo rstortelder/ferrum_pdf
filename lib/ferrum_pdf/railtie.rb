54f6fca919cf5e8bdbe698e592631819f976a2c2
Rob Stortelder <rob@stortelder.eu>
2025-03-25 13:02:23 +0100

    Try including the assets helper in the railtie

--- lib/ferrum_pdf/railtie.rb
+++ lib/ferrum_pdf/railtie.rb
@@ -2,9 +2,12 @@ module FerrumPdf
   class Railtie < ::Rails::Railtie
     initializer "ferrum_pdf.controller" do
       ActiveSupport.on_load(:action_controller) do
-        include FerrumPdf::AssetsHelper if FerrumPdf.include_assets_helper_module
         include FerrumPdf::Controller if FerrumPdf.include_controller_module
       end
+    end
+
+    initializer "ferrum_pdf.helpers" do
+      ActionView::Base.include(FerrumPdf::AssetsHelper)
     end
   end
 end
